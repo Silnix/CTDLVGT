ff41fa6d2d33955e46cbdf80a0e3f19fde66572b
Silnix <116791213+Silnix@users.noreply.github.com>
2023-04-18 10:06:39 +0700

    Add files via upload

--- TH1nc.cpp
+++ TH1nc.cpp
@@ -1,8 +1,10 @@
 #include <stdio.h>
 void input (int a[], int n);
 void output (int a[], int n);
-int LinearSearch(int a[], int n, int key);
-//void SelectionSort (int a[], int n);
+int LinearSearch (int a[], int n, int keyL);
+int BinarySearch (int a[], int n, int keyB);
+void SelectionSort (int a[], int n);
+void InsertionSort (int a[], int n);
 int main() {
 	int n;
   		printf("Nhap so phan tu cua mang: ");
@@ -10,20 +12,35 @@ int main() {
 	int a[n];
 
 	input(a,n);
+		printf("Mang vua nhap la:\n");
 	output(a,n);
 	
-	int key;
-    printf("\nNhap gia tri can tim kiem: ");
-    scanf("%d", &key);
-    int index = LinearSearch(a, n, key);
-    if (index == -1) {
-        printf("Khong tim thay gia tri %d trong mang.\n", key);
+	int keyL;
+    printf("\nNhap gia tri can tim kiem bang LinearSearch: ");
+    scanf("%d", &keyL);
+    int indexL = LinearSearch(a, n, keyL);
+    if (indexL == -1) {
+        printf("Khong tim thay gia tri %d trong mang.\n", keyL);
     } else {
-        printf("Gia tri %d duoc tim thay tai vi tri %d trong mang.\n", key, index);
-    	}
-			
-//	SelectionSort(a,n);
+        printf("Gia tri %d duoc tim thay tai vi tri %d trong mang.\n", keyL, indexL);
+    }
+    
+    int keyB;
+    printf("\nNhap gia tri can tim kiem bang BinarySearch: ");
+    scanf("%d", &keyB);
+    int indexB = BinarySearch(a, n, keyB);
+    if (indexB == -1) {
+        printf("Khong tim thay gia tri %d trong mang.\n", keyB);
+    } else {
+        printf("Gia tri %d duoc tim thay tai vi tri %d trong mang.\n", keyB, indexB);
+    }
 
+    printf("\nMang sau khi duoc Selection Sort: \n");			
+	SelectionSort(a,n);
+	output(a,n);
+	printf("\nMang sau khi duoc Insertion Sort: \n");
+	InsertionSort(a,n);
+	output(a,n);
 
 return 0;
 }
@@ -38,21 +55,34 @@ void input (int a[], int n) {
 
 void output (int a[], int n) {
 	int *p;
-
-	printf("Mang vua nhap la:\n");
+	
   	for (p = a; p < a + n; p++) {
     	printf("%4d ", *p);
   	}
 }
-int LinearSearch(int a[], int n, int key) {
+int LinearSearch(int a[], int n, int keyL) {
     for (int i = 0; i < n; i++) {
-        if (a[i] == key) {
+        if (a[i] == keyL) {
             return i;
         }
     }
     return -1;
 }
-/*
+int BinarySearch (int a[], int n, int keyB) {
+    int left = 0;
+    int right = n - 1;
+    while (left <= right) {
+        int mid = (left + right) / 2;
+        if (a[mid] == keyB) {
+            return mid;
+        } else if (a[mid] < keyB) {
+            left = mid + 1;
+        } else {
+            right = mid - 1;
+        }
+    }
+    return -1;
+}
 void SelectionSort (int a[], int n) {
 	int i, j, minIndex, temp;
 
@@ -67,7 +97,16 @@ void SelectionSort (int a[], int n) {
     	a[i] = a[minIndex];
     	a[minIndex] = temp;
   	}
-  printf("Mang sau khi duoc Selection Sort: \n");
 }
-*/
+void InsertionSort (int a[], int n) {
+    for (int i = 1; i < n; i++) {
+        int key = a[i];
+        int j = i - 1;
+        while (j >= 0 && a[j] > key) {
+            a[j + 1] = a[j];
+            j--;
+        }
+        a[j + 1] = key;
+    }
+}
 
